96f472419eab05229a5878cafa00aea6f865aeee
Tom Mortimer-Jones <tom@morty.co.uk>
2014-07-24 10:17:15 +0100

    Added default args to pager_url
    Allows handling both signatures in the paging template

--- ckanext/harvest/lib.py
+++ ckanext/harvest/lib.py
@@ -3,7 +3,7 @@ from pylons import url as _pylons_default_url
 class HarvestError(Exception):
     pass
 
-def pager_url(ignore, page):
+def pager_url(ignore=None, page=None):
     # This has params the same as ckan.controllers.Package.pager_url NOT
     # ckan.helpers.pager_url
     routes_dict = _pylons_default_url.environ['pylons.routes_dict']
