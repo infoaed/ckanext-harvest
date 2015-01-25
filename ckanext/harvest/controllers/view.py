60f3b3918addc8d933746b3ef07b99dbbd92e0b7
heinlja <jaanus.heinlaid@gmail.com>
2015-01-25 16:56:10 +0200

    Revert "Estonianising."
    Revert commits made after 035d1e02196aa4051be40b71882dbb065a51126a.

--- ckanext/harvest/controllers/view.py
+++ ckanext/harvest/controllers/view.py
@@ -1,5 +1,3 @@
-﻿# -*- coding: utf-8 -*-
-
 import re
 from lxml import etree
 from lxml.etree import XMLSyntaxError
@@ -230,17 +228,16 @@ class ViewController(BaseController):
         try:
             context = {'model':model, 'user':c.user, 'session':model.Session}
             p.toolkit.get_action('harvest_job_create')(context,{'source_id':id})
-            refresh_interval_min = config.get('ckan.harvest.refresh_interval_min', '15')
-            h.flash_success(u'Värskendus tellitud. Järgmine andmekorje toimub mitte hiljem kui %s minuti pärast.', refresh_interval_min)
+            h.flash_success(_('Refresh requested, harvesting will take place within 15 minutes.'))
         except p.toolkit.ObjectNotFound:
-            abort(404,_('Andmekorje allikat ei leitud'))
+            abort(404,_('Harvest source not found'))
         except p.toolkit.NotAuthorized,e:
             abort(401,self.not_auth_message)
         except HarvestError, e:
-            msg = 'Viga andmekorje tellimuse loomisel: %s' % str(e)
+            msg = 'Could not create harvest job: %s' % str(e)
             h.flash_error(msg)
         except Exception, e:
-            msg = 'Tehniline viga: [%s]' % str(e)
+            msg = 'An error occurred: [%s]' % str(e)
             h.flash_error(msg)
 
         redirect(h.url_for('harvest'))
